2294e235aff7237b22cc13ff6b7be6cfbc7b6f8d
Oscar Bruun Enebo <oscar.enebo@gmail.com>
2026-09-24 09:35:31 +0200

    Create total_annual_energy_consumption_dk.py

---
+++ total_annual_energy_consumption_dk.py
@@ -0,0 +1,11 @@
+# ALL UNITS IN PETAJOULES (PJ)
+
+TOTAL = 730
+HOUSEHOLDS = 300   #  including commercial and public services
+INDUSTRY = 150+30  # including agriculture and 30PJ of data centers expected by 2030
+LIGHT_TRANSPORT = 100
+
+# HEAVY TRANSPORT
+TRUCKS = 50
+SHIPPING = 50      # not including 500 PJ of international shipping under Danish flag
+AVIATION = 50
